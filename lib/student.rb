5d6def469fd512976cb9de6de61746f643a2d7f4
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 03:57:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   
   def self.create_table
     sql = <<- SQL
-      CREATE TABLE student (
+      CREATE TABLE IF NOT EXISTS student (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
