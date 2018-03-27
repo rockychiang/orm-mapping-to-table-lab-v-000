2b063b78a55dc845c4cc577c41b0215b5cfa7e14
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 03:49:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,9 @@
 class Student
   attr_accessor :name, :grade, :id
   
+  def self.create_table
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
