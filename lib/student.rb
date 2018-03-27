404fdab1394a12225297b78e75b33a9d00dfb36d
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 04:01:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,9 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  
+  def save
+    
+  end
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
